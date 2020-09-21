1b3c28bbf01306554d6c2d6b40976fbf0568ed3d
Florian Tenzer <florian.tenzer@tum.de>
2020-09-21 21:13:06 +0200

    added default statement

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', '5yljr14wn6epx4+zpjj)ep@dungvkx7+nim!83)^b!p60#h8pv')
+SECRET_KEY = env('SECRET_KEY', default='5yljr14wn6epx4+zpjj)ep@dungvkx7+nim!83)^b!p60#h8pv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
