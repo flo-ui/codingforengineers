f3c6e7f717f29b766313b7bfe806e29b3ac02bea
Florian Tenzer <florian.tenzer@tum.de>
2020-09-21 21:35:48 +0200

    new allowed host

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY', default='5yljr14wn6epx4+zpjj)ep@dungvkx7+nim!83)^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['codingforengineers.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['codingforengineers.herokuapp.com', 'cfengineers-stage.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
