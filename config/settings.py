168694a9811be23805b8465741c5467ff7d856bd
Florian Tenzer <florian.tenzer@tum.de>
2020-10-04 11:07:48 +0200

    change local and deploym. db to postgres

--- config/settings.py
+++ config/settings.py
@@ -91,10 +91,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cfengineers',
+        'USER': 'floui',
+        'PASSWORD': 'sp7a81z',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+    
 
 
 # Password validation
@@ -143,3 +149,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static')
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
